4fcaf3d179c89e16a311b8586357b58d579a4948
Tony Yip <tony@tonyhhyip.me>
2019-01-10 19:36:01 +0800

    Add allow no password

--- pkg/http/restful/middleware.go
+++ pkg/http/restful/middleware.go
@@ -4,11 +4,20 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
-var authMiddleware gin.HandlerFunc
+var authMiddleware gin.HandlerFunc = func(c *gin.Context) {
+	c.Next()
+}
 
 func init() {
-	accounts := gin.Accounts{os.Getenv("AUTH_BASIC_USERNAME"): os.Getenv("AUTH_BASIC_PASSWORD")}
+	username := os.Getenv("AUTH_BASIC_USERNAME")
+	password := os.Getenv("AUTH_BASIC_PASSWORD")
+	if username == "" || password == "" {
+		logrus.Warnln("Username and Password is not config, please set environment variable AUTH_BASIC_USERNAME and AUTH_BASIC_PASSWORD")
+		return
+	}
+	accounts := gin.Accounts{username: password}
 	authMiddleware = gin.BasicAuth(accounts)
 }
